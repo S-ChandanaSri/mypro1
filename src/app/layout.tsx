71cbc4e1b44eb4e0fd58b98cf6b98db683be7373
Pranansh Singh <prananshsingh@Prananshs-MacBook-Air.local>
2024-08-13 20:37:42 +0530

    Initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,23 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Poppins } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+const poppins = Poppins({
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-poppins",
+  subsets: ["latin"],
+});
+const helveticaWorld = localFont({
+  src: "../../public/fonts/HelveticaWorld.woff2",
+  variable: "--font-helvetica",
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +31,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`${inter.variable} ${poppins.variable} ${helveticaWorld.variable}`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
