be6a38d48ae477c6a08b74da808b3bcc2868e42c
Rajchaudhary860 <rkraj9905@gmail.com>
2024-10-26 23:50:41 +0530

    commit

--- declaration.d.ts
+++ declaration.d.ts
@@ -13,4 +13,8 @@ declare module "*.png" {
 declare module "*.jpg" {
     const content: any;
     export default content;
-}  
+} 
+declare module 'react';
+declare module 'react-dom';
+declare module 'react-select';
+declare module 'react-select-country-list';
